277f057cc51bfcdc4940076c860c24521baef821
Aman Dubey <amandubey042004@gmail.com>
2024-09-20 21:44:05 +0530

    reesl

--- client/src/Reelsshow/Reelsshow.js
+++ client/src/Reelsshow/Reelsshow.js
@@ -1,27 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Nav2 from '../Body/Nav2/Nav2'
+import axios from 'axios'
 
 const Reelsshow = () => {
+    const [reel, setreel] = useState([])
+    const reels = async () => {
+        const data = await axios.get("http://127.0.0.1:8000/getpostall");
+        setreel(data.data)
+    }
+    useEffect(() => {
+        reels()
+    })
     return (
         <div className='w-full h-auto bg-white text-black'>
 
             <div className='flex'>
                 <Nav2 />
-                <div>
-                <div className='w-[350px] h-[560px] ml-[600px] mt-6  bg-red-500 rounded-2xl'>
+                <div className='snap-y snap-mandatory h-[100vh] overflow-y-scroll relative'>
+                    {
+                        reel.map((data, i) => (
+                            <div className='snap-center w-[350px] h-[560px] ml-[600px] mt-6 mb-10 bg-black rounded-2xl relative'>
+                                <img src={`http://127.0.0.1:8000/uploads/${data.photos}`} alt=""  className='rounded-2xl h-full cursor-pointer opacity-[0.5]' />
+                                <p className='absolute bottom-10 left-10 text-white text-2xl font-normal'>{data.caption}</p>
+                                {/* <p className='absolute bottom-10 left-10 text-white text-2xl font-normal'>{data.location}</p> */}
+                            </div>
+                        ))
+                    }
 
-                    <img src='./image/ban2.jpg' alt="" className='rounded-2xl object-cover w-[350px] h-[560px] cursor-pointer w-60 h-64' />
-
-                </div>
-                <div className='w-[350px] h-[560px] ml-[600px] mt-6  bg-red-500 rounded-2xl'>
-
-                    <img src='./image/ban2.jpg' alt="" className='rounded-2xl object-cover w-[350px] h-[560px] cursor-pointer w-60 h-64' />
-
-                </div><div className='w-[350px] h-[560px] ml-[600px] mt-6  bg-red-500 rounded-2xl'>
-
-                    <img src='./image/ban2.jpg' alt="" className='rounded-2xl object-cover w-[350px] h-[560px] cursor-pointer w-60 h-64' />
-
-                </div>
                 </div>
             </div>
         </div>

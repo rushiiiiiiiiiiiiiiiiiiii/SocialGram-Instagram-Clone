9e535b14372fe0e2ca09cb109081766fcb4c7900
Aman Dubey <amandubey042004@gmail.com>
2024-09-23 14:35:01 +0530

    story works

--- client/src/Body/Story/Story.js
+++ client/src/Body/Story/Story.js
@@ -13,37 +13,38 @@ const Story = () => {
   var id = sessionStorage.getItem("userid")
   const [name, setName] = useState()
   const [uname, setUame] = useState()
- const[photo,setphoto]=useState()
-  const[sdata,setSdata]= useState([])
-  const[file,setFile]= useState(null)
+  const [photo, setphoto] = useState()
+  const [sdata, setSdata] = useState([])
+  const [file, setFile] = useState(null)
   const [open, setOpen] = useState(false);
   const [showcom, setShowcom] = useState();
 
 
-  const getdata = ()=>{
+  const getdata = () => {
     axios.get("http://127.0.0.1:8000/getuserall")
-    .then(res => {
-      setSdata(res.data)})
-    .catch(err => console.log(err))
-   }
-   
-   useEffect(() => {
+      .then(res => {
+        setSdata(res.data)
+      })
+      .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
     getdata()
   }, [])
   const userid = sessionStorage.getItem("userid")
-  const showpost = ()=>{
-    axios.get("http://127.0.0.1:8000/getuser/"+ userid)
-    .then(res=>{
+  const showpost = () => {
+    axios.get("http://127.0.0.1:8000/getuser/" + userid)
+      .then(res => {
         setName(res.data[0].name)
         setUame(res.data[0].username)
         setphoto(res.data[0].photos)
-    })
-    .catch(err=>console.log(err))
-}
-useEffect(()=>{
+      })
+      .catch(err => console.log(err))
+  }
+  useEffect(() => {
     showpost()
 
-},[userid])
+  }, [userid])
   const getstoryall = () => {
     axios.get("http://127.0.0.1:8000/getstory/" + id)
       .then(res => {
@@ -55,9 +56,11 @@ useEffect(()=>{
     getstoryall()
   }, [])
   const getstoryalluser = () => {
-    axios.get("http://127.0.0.1:8000/getstoryall/"+id)
+    axios.get("http://127.0.0.1:8000/followerStory/" + id)
       .then(res => {
         setStoryalluser(res.data)
+        console.log(res.data)
+
       })
       .catch(err => console.log(err))
   }
@@ -67,7 +70,7 @@ useEffect(()=>{
   const showaddstory = () => {
     setShowaddst(true)
   }
-  
+
   const showus = (postid) => {
     setOpen(true);
     setShowcom(postid);
@@ -78,94 +81,80 @@ useEffect(()=>{
         showaddst ?
           <Storyupload setShowaddst={setShowaddst} getstory={getstoryall} /> : ""
       }
- {
+      {
         open ?
           <Storyview setShowcom={setOpen} getstoryid={showcom} /> : ""
       }
 
       <div className="box-story w-24 h-40 bg-loww rounded-xl ml-[1px] relative cursor-pointer">
         {
-          storyd.length>0?
-          storyd.map((data, i) =>{
-          const user= sdata.find(user=> user.id === data.sid)
+          storyd.length > 0 ?
+            storyd.map((data, i) => {
+              const user = sdata.find(user => user.id === parseInt(data.sid))
+              return (
+                <div className='w-24 h-40' key={i}>
+                  <img src={`http://127.0.0.1:8000/uploads/${user?.photos}`} className="h-[50px] w-[50px] border-4 border-gray-500  rounded-full z-10 absolute top-1 left-2" alt="" />
 
-          return(
-            <div className='w-24 h-40' key={i}>
-        <img src={`http://127.0.0.1:8000/uploads/${user?.photos}`} className="h-[50px] w-[50px] border-4 border-gray-500  rounded-full z-10 absolute top-1 left-2" alt="" />
+                  <img src={`http://127.0.0.1:8000/uploads/${data.photos}`} alt="" className='w-24 h-40 object-cover rounded-xl' />
+                </div>
+              )
+            }) :
+            ""
+        }
 
-              <img src={`http://127.0.0.1:8000/uploads/${data.photos}`} alt="" className='w-24 h-40 object-cover rounded-xl' />
-            </div>
-      )}):
-      ""
-    }
-        {/* <h1 className='-mt-11 ml-2 text-[14px] font-semibold text-black'>{name}</h1> */}
-    
         {
-        storyd.length <= 0 ?
+          storyd.length <= 0 ?
 
             <div className='mt-[px]'>
-        <img src={`http://127.0.0.1:8000/uploads/${photo}`} className="h-10 w-10 rounded-full z-10 absolute top-2 left-2" alt="" />
+              <img src={`http://127.0.0.1:8000/uploads/${photo}`} className="h-10 w-10 rounded-full z-10 absolute top-2 left-2" alt="" />
 
               <h1 class="text  ml-7 border-gray-300 rounded border-none  font-semibold cursor-pointer p-1 w-[50px] mt-[90px] text-center text-3xl"><FaPlus onClick={showaddstory} /></h1>
 
-              {/* <h1 className='mt-4 text-center  p-0 text-sm'>{name}</h1> */}
-            </div>:""
-          }
-         
-       
-      </div>
- 
-
-      <div className="box-story w-24 flex gap-3  h-40 bg-loww rounded-xl ml-[1px] cursor-pointer">
-        {
-          storyalluser.length>0?
-          storyalluser.slice().reverse().map((data, i) =>{
-            const user = sdata.find(user=> user.id === data.sid)
-              return(
-            <div key={i}>
-            <div className='w-24 h-40 relative' onClick={()=>showus(data.id)}>
-              <img src={`http://127.0.0.1:8000/uploads/${user?.photos}`}  className='h-[50px] w-[50px] rounded-full border-4 border-red-500 z-10 absolute top-1 left-2' alt="" />
-              <img src={`http://127.0.0.1:8000/uploads/${data.photos}`} alt="" className='w-24 h-40 object-cover rounded-xl' />
-            
-              {/* <h1  className='h-10 w-10 rounded-full z-50 pt-20 absolute top-2 left-2' alt="" >Rushikesh Arote</h1> */}
-            </div>
-            </div>
-          )}):""
+            </div> : ""
         }
 
-    <div className="story mt-6 flex gap-3 overflow-x-hidden w-[638px]">
-      
-      <div className="box-story w-24 h-40 bg-loww rounded-xl ml-[1px] relative">
-      <img src="/image/prof.jpg" className="h-10 w-10 rounded-full z-30 absolute top-2 left-2" alt="" />
-      
-      {/* {
-      storyd?
-         storyd.map((data,i)=>(
-          <div key={i}>
-        <img src={`http://127.0.0.1:8000/uploads/${data.photos}`} alt="/" className='rounded-xl cursor-pointer object-cover w-24 h-40 ' />
-      
+
       </div>
-       )):"no story"
-      } */}
-      { 
-      storyd.length<=0 ?     
-      <div className='mt-9' >
-      <label className='ml-10'>
-          <input name='photo' onChange={e=>setFile(e.target.files[0])}  class="text-sm ml-5 cursor-pointer border-none  w-[50px] hidden" type="file" multiple />
-          <div class="text  ml-7 border-gray-300 rounded border-none -mt-3 font-semibold cursor-pointer p-1 w-[50px] text-center text-3xl"><FaPlus/></div>
-        </label>
-        {/* <button onClick={create} className='w-10 h-5 bg-indigo-500 ml-7 text-center text-sm'>add</button> */}
-        <h1 className='mt- text-center flex p-0 text-sm'>Rushikesh Arote</h1>
-        </div>:
-        ""
-}
+
+
+      <div >
+        {
+
+          storyalluser!=="no followers found" ?
+            storyalluser.map((data, i) => {
+              const user = sdata.find(user => user.id === parseInt(data.sid))
+              return (
+                <div key={i} className="box-story w-24 flex gap-3  h-40 bg-loww rounded-xl ml-[1px] cursor-pointer">
+                  <div className='w-24 h-40 relative' onClick={() => showus(data.id)}>
+                    <img src={`http://127.0.0.1:8000/uploads/${user?.photos}`} className='h-[50px] w-[50px] rounded-full border-4 border-red-500 z-10 absolute top-1 left-2' alt="" />
+                    <img src={`http://127.0.0.1:8000/uploads/${data.photos}`} alt="" className='w-24 h-40 object-cover rounded-xl' />
+
+                  </div>
+                </div>
+              )
+            }) : ""
+        }
+
+        <div>
+
+          {/* <div className="box-story w-24 h-40 bg-loww rounded-xl ml-[1px] relative">
+            {storyd.length <= 0 ?
+              <div className='mt-9 story flex gap-3 overflow-x-hidden w-[638px]' >
+                <label className='ml-10'>
+                  <input name='photo' onChange={e => setFile(e.target.files[0])} class="text-sm ml-5 cursor-pointer border-none  w-[50px] hidden" type="file" multiple />
+                  <div class="text  ml-7 border-gray-300 rounded border-none -mt-3 font-semibold cursor-pointer p-1 w-[50px] text-center text-3xl"><FaPlus /></div>
+                </label>
+                <h1 className='mt- text-center flex p-0 text-sm'>Rushikesh Arote</h1>
+              </div> :
+              ""
+            }
+
+          </div> */}
 
         </div>
+      </div>
 
     </div>
-</div>
-
-</div>
   )
 }
 
--- server/server.js
+++ server/server.js
@@ -5,60 +5,48 @@ var multer = require('multer')
 var con = require('./conn')
 app.use(cors())
 app.use(express.json())
-
-
-// app.post('/register', (req,res)=>{
-//     var sql = 'INSERT INTO login(name,username,email,password) VALUES(?,?,?,?)'
-//     var {name,uname,email,password} = req.body;
-      
-//     con.query(sql,[name,uname,email,password],(err,result)=>{
-//         if(err) return res.json(err)
-//             return res.json(result);
-//     })
-// })
-
-app.post('/login', (req,res)=>{
+app.post('/login', (req, res) => {
   var sql = 'SELECT * FROM login WHERE email = ? AND password = ?'
-  var {email,password} = req.body;
-  con.query(sql,[email,password],(err,result)=>{
-    if(err) return res.json(err)
-      // console.log(result[0].id)
-      if(result.length>0){
-        const sql='UPDATE login SET IsLogin = 1 WHERE id = ?';
-        con.query(sql,[result[0].id],(err,response)=>{
-          if(err) return res.json(err)
-            return res.json(result);
-        })
-      }
+  var { email, password } = req.body;
+  con.query(sql, [email, password], (err, result) => {
+    if (err) return res.json(err)
+    // console.log(result[0].id)
+    if (result.length > 0) {
+      const sql = 'UPDATE login SET IsLogin = 1 WHERE id = ?';
+      con.query(sql, [result[0].id], (err, response) => {
+        if (err) return res.json(err)
+        return res.json(result);
+      })
+    }
   })
 })
 
-app.post('/logout/:id',(req,res)=>{
-  const id=req.params.id
-  const sql='UPDATE login SET IsLogin = 0 WHERE id = ?'
-  con.query(sql,[id],(err,result)=>{
-    if(err) return res.json(err)
-      return res.json(result);
+app.post('/logout/:id', (req, res) => {
+  const id = req.params.id
+  const sql = 'UPDATE login SET IsLogin = 0 WHERE id = ?'
+  con.query(sql, [id], (err, result) => {
+    if (err) return res.json(err)
+    return res.json(result);
   })
 })
 
-app.use('/uploads', express.static('uploads'))                                                           
+app.use('/uploads', express.static('uploads'))
 
 const storage = multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null, './uploads')
-    },
-    filename:(req,file,cb)=>{
-        cb(null,`image-${Date.now()}${file.originalname}`)
-    }
+  destination: (req, file, cb) => {
+    cb(null, './uploads')
+  },
+  filename: (req, file, cb) => {
+    cb(null, `image-${Date.now()}${file.originalname}`)
+  }
 })
 
 const upload = multer({
-    storage:storage
+  storage: storage
 })
 const data = multer()
 
-app.post('/register', upload.single("photo"),(req,res)=>{
+app.post('/register', upload.single("photo"), (req, res) => {
   var sql = 'INSERT INTO login(photos,name,username,email,password,IsLogin) VALUES(?,?,?,?,?,0)'
   var filename = req.file.filename;
   var name = req.body.name;
@@ -66,39 +54,39 @@ app.post('/register', upload.single("photo"),(req,res)=>{
   var email = req.body.email;
   var password = req.body.password;
 
-  con.query(sql,[filename,name,uname,email,password],(err,result)=>{
-      if(err) return res.json(err)
-          return res.json(result);
+  con.query(sql, [filename, name, uname, email, password], (err, result) => {
+    if (err) return res.json(err)
+    return res.json(result);
   })
 })
 
-app.post('/create/:id',upload.single("photo"),(req,res)=>{
-  var sql ='INSERT INTO addpost(sid,caption,location,photos) VALUES(?,?,?,?)'
+app.post('/create/:id', upload.single("photo"), (req, res) => {
+  var sql = 'INSERT INTO addpost(sid,caption,location,photos) VALUES(?,?,?,?)'
   var sid = req.body.id;
   var caption = req.body.caption;
   var location = req.body.location;
   var filename = req.file.filename;
   // console.log(filename)
-  con.query(sql,[sid,caption,location,filename],(err,result)=>{
-    if(err)  return res.json(err)
-      return res.json(result)
+  con.query(sql, [sid, caption, location, filename], (err, result) => {
+    if (err) return res.json(err)
+    return res.json(result)
   })
 })
-app.post('/createstory/:id',upload.single("photo"),(req,res)=>{
-  var sql ='INSERT INTO storytb(sid,photos) VALUES(?,?)'
+app.post('/createstory/:id', upload.single("photo"), (req, res) => {
+  var sql = 'INSERT INTO storytb(sid,photos) VALUES(?,?)'
   var sid = req.body.id;
   var filename = req.file.filename;
 
 
-  con.query(sql,[sid,filename],(err,result)=>{
-    if(err)  return res.json(err)
-      return res.json(result)
+  con.query(sql, [sid, filename], (err, result) => {
+    if (err) return res.json(err)
+    return res.json(result)
   })
 })
 app.get('/getpost/:id', (req, res) => {
   const sql = "SELECT * FROM addpost WHERE sid=?";
   const sid = req.params.id;
- // Correct variable name
+  // Correct variable name
 
   con.query(sql, [sid], (err, result) => {
     if (err) return res.json(err);
@@ -124,10 +112,10 @@ app.get('/getstorypic/:id', (req, res) => {
   });
 });
 app.get('/getstoryall/:id', (req, res) => {
-  const id =parseInt(req.params.id);
+  const id = parseInt(req.params.id);
   const sql = "SELECT * FROM storytb WHERE sid != ? ";
 
-  con.query(sql,[id],(err, result) => {
+  con.query(sql, [id], (err, result) => {
     if (err) return res.json(err);
     return res.json(result);
   });
@@ -171,39 +159,39 @@ app.get('/getpostcom/:id', (req, res) => {
   var id = req.params.id;
   const sql = "SELECT * FROM addpost WHERE id=?";
 
-  con.query(sql, [id],(err, result) => {
+  con.query(sql, [id], (err, result) => {
     if (err) return res.json(err);
     return res.json(result);
   });
 });
-app.post('/like/:id',(req,res)=>{
+app.post('/like/:id', (req, res) => {
   var sid = req.body.id;
   var pid = req.body.pid;
   var like = req.body.likes;
-  var sqls=`SELECT * FROM liketb WHERE sid=${sid} and pid=${pid}`
-  con.query(sqls,(err,result)=>{
-    if(err) return err;
-      if(result.length>0){
-  var sql ='DELETE FROM liketb WHERE sid=? AND pid=?'
-    con.query(sql,[sid,pid],(err,result)=>{
-      if(err)  return res.json(err);
+  var sqls = `SELECT * FROM liketb WHERE sid=${sid} and pid=${pid}`
+  con.query(sqls, (err, result) => {
+    if (err) return err;
+    if (result.length > 0) {
+      var sql = 'DELETE FROM liketb WHERE sid=? AND pid=?'
+      con.query(sql, [sid, pid], (err, result) => {
+        if (err) return res.json(err);
         return res.json(result)
-    })
-  }
-  else{
-  var sql ='INSERT INTO liketb(sid,pid,likes) VALUES(?,?,?)'    
-  con.query(sql,[sid,pid,like],(err,result)=>{
-    if(err)  return res.json(err)
-      return res.json(result)
+      })
+    }
+    else {
+      var sql = 'INSERT INTO liketb(sid,pid,likes) VALUES(?,?,?)'
+      con.query(sql, [sid, pid, like], (err, result) => {
+        if (err) return res.json(err)
+        return res.json(result)
+      })
+    }
   })
-}
- })
 })
-app.get('/chatuser/:id',async (req,res) => {
-  const id=req.params.id;
-  var sql="SELECT * FROM login WHERE id <> ?"
-  con.query(sql,[id],(err,result)=>{
-    if(err) return err;
+app.get('/chatuser/:id', async (req, res) => {
+  const id = req.params.id;
+  var sql = "SELECT * FROM login WHERE id <> ?"
+  con.query(sql, [id], (err, result) => {
+    if (err) return err;
     return res.json(result)
   })
 })
@@ -221,116 +209,152 @@ app.get('/getlike/:id', (req, res) => {
       return res.json(result[0]); // The dynamic alias will be the key of the result
     } else {
       return res.status(404).json({ message: 'No likes found for the given post ID' });
-}
+    }
+  });
 });
-});
-app.get('/getsave/:id',(req,res)=>{
+app.get('/getsave/:id', (req, res) => {
   const id = req.params.id
   const sql = "SELECT * FROM savetb WHERE sid=?"
-  con.query(sql,[id],(err,result)=>{
-    if(err) return res.json(err)
-      return res.json(result)
+  con.query(sql, [id], (err, result) => {
+    if (err) return res.json(err)
+    return res.json(result)
   })
 })
-app.get('/getcomment/:id',(req,res)=>{
+app.get('/getcomment/:id', (req, res) => {
   var id = req.params.id;
   const sql = "SELECT * FROM commenttb WHERE pid=?"
-  con.query(sql,[id],(err,result)=>{
-    if(err) return res.json(err)
-      return res.json(result)
+  con.query(sql, [id], (err, result) => {
+    if (err) return res.json(err)
+    return res.json(result)
   })
 })
-app.get('/commentlength/:id',async(req,res)=>{
-  const ids=req.params.id
+app.get('/commentlength/:id', async (req, res) => {
+  const ids = req.params.id
   const sql = `SELECT count(*) AS \`${ids}\` FROM commenttb WHERE pid = ?`;
-  con.query(sql,[ids],(err,result)=>{
-    if(err)return err;
+  con.query(sql, [ids], (err, result) => {
+    if (err) return err;
     return res.json(result[0]);
   })
 })
-app.post('/comment/:id',(req,res)=>{
+app.post('/comment/:id', (req, res) => {
   var sid = req.body.id;
   var pid = req.body.pid;
   var comment = req.body.comment;
-  var sql ='INSERT INTO commenttb(sid,pid,comment) VALUES(?,?,?)'
+  var sql = 'INSERT INTO commenttb(sid,pid,comment) VALUES(?,?,?)'
 
-  
-  con.query(sql,[sid,pid,comment],(err,result)=>{
-    if(err)  return res.json(err)
-     return res.json(result)
+
+  con.query(sql, [sid, pid, comment], (err, result) => {
+    if (err) return res.json(err)
+    return res.json(result)
   })
 })
-app.post('/save/:id',(req,res)=>{
+app.post('/save/:id', (req, res) => {
   var sid = req.body.id;
   var pid = req.body.pid;
   var save = req.body.save;
-  var sqls=`SELECT * FROM savetb WHERE sid=${sid} and pid=${pid}`
-  con.query(sqls,(err,result)=>{
-    if(err) return err;
-      if(result.length>0){
-  var sql ='DELETE FROM savetb WHERE sid=? AND pid=?'
-    con.query(sql,[sid,pid],(err,result)=>{
-      if(err)  return res.json(err);
+  var sqls = `SELECT * FROM savetb WHERE sid=${sid} and pid=${pid}`
+  con.query(sqls, (err, result) => {
+    if (err) return err;
+    if (result.length > 0) {
+      var sql = 'DELETE FROM savetb WHERE sid=? AND pid=?'
+      con.query(sql, [sid, pid], (err, result) => {
+        if (err) return res.json(err);
         return res.json(result)
-    })
-  }
-  else{
-  var sql ='INSERT INTO savetb(sid,pid,save) VALUES(?,?,?)'    
-  con.query(sql,[sid,pid,save],(err,result)=>{
-    if(err)  return res.json(err)
-      return res.json(result)
+      })
+    }
+    else {
+      var sql = 'INSERT INTO savetb(sid,pid,save) VALUES(?,?,?)'
+      con.query(sql, [sid, pid, save], (err, result) => {
+        if (err) return res.json(err)
+        return res.json(result)
+      })
+    }
   })
-}
- })
 })
-app.post('/SendMess',async(req,res)=>{
-  const {Message,ReceiverID,SenderID}=req.body
-  var sql ='INSERT INTO chats(SenderID,ReceiverID,Message) VALUES(?,?,?)'  
-  con.query(sql,[SenderID,ReceiverID,Message],(err,result)=>{
-    if(err) return err;
+app.post('/SendMess', async (req, res) => {
+  const { Message, ReceiverID, SenderID } = req.body
+  var sql = 'INSERT INTO chats(SenderID,ReceiverID,Message) VALUES(?,?,?)'
+  con.query(sql, [SenderID, ReceiverID, Message], (err, result) => {
+    if (err) return err;
     return res.json(result)
-  })  
+  })
 })
-app.get('/Message',async(req,res)=>{
-  const{userId,id}=req.query
-  const sql=` SELECT * FROM chats
+app.get('/Message', async (req, res) => {
+  const { userId, id } = req.query
+  const sql = ` SELECT * FROM chats
     WHERE (SenderID = ? AND ReceiverID = ?)
     OR (SenderID = ? AND ReceiverID = ?)
     `
-  con.query(sql,[userId,id,id,userId],(err,result)=>{
+  con.query(sql, [userId, id, id, userId], (err, result) => {
     // console.log(userId,id)
-    if(err) return err;
+    if (err) return err;
     return res.json(result)
   })
 })
-app.post('/follow',(req,res)=>{
-  const{userId,id}=req.body
-  console.log(userId,id);
-  const sql ='INSERT INTO follower(follower,following) VALUES(?,?)';
-   con.query(sql,[id,userId],(err,result)=>{
-    if(err) return err;
+app.post('/follow', (req, res) => {
+  const { userId, id } = req.body
+  console.log(userId, id);
+  const sql = 'INSERT INTO follower(follower,following) VALUES(?,?)';
+  con.query(sql, [id, userId], (err, result) => {
+    if (err) return err;
     return res.json(result)
   })
 })
-app.get('/getfollower',async(req,res)=>{
-  const{userId,id}=req.query
-  const sql=`SELECT * FROM follower
+app.get('/getfollower', async (req, res) => {
+  const { userId, id } = req.query
+  const sql = `SELECT * FROM follower
     WHERE (follower = ?)
     OR (following = ?)`
-  con.query(sql,[id,id],(err,result)=>{
-    if(err) return err;
+  con.query(sql, [id, id], (err, result) => {
+    if (err) return err;
     return res.json(result)
   })
 })
-app.delete('/unfollow/:id',async(req,res)=>{
-  const id=req.params.id;
-  const sql="DELETE FROM follower WHERE following=?";
-  con.query(sql,[id],(err,result)=>{
-    if(err) return err;
+app.delete('/unfollow/:id', async (req, res) => {
+  const id = req.params.id;
+  const sql = "DELETE FROM follower WHERE following=?";
+  con.query(sql, [id], (err, result) => {
+    if (err) return err;
     return res.json(result)
   })
 })
-app.listen(8000, ()=>{
-  console.log("server running on 8000")
+app.get('/followerStory/:id', (req, res) => {
+  const id = req.params.id;
+  const sql = "SELECT * FROM follower WHERE following = ?";
+
+  con.query(sql, [id], (err, followers) => {
+    if (err) return res.status(500).json({ error: err.message });
+
+    if (followers.length === 0) {
+      return res.json("no followers found");
+    }
+
+    const storiesPromises = followers.map(follower => {
+      return new Promise((resolve, reject) => {
+        const storySql = "SELECT * FROM storytb WHERE sid = ?";
+        con.query(storySql, [follower.follower], (err, storyResults) => {
+          if (err) return reject(err);
+          resolve(storyResults);
+        });
+      });
+    });
+
+    Promise.all(storiesPromises)
+      .then(allStories => {
+        const flatStories = allStories.flat();
+        if (flatStories.length > 0) {
+          res.json(flatStories);
+        } else {
+          res.json("no story found");
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+});
+
+app.listen(8000, () => {
+  console.log("server running on 8000");
 })
 // AIzaSyCUquQJirgRWIJ9Bn7nmXt6BAvxMhK6L2Y
